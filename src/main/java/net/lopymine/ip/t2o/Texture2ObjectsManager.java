a5c4ff9aa5769e933d3946312602beaf3fb5fb56
LopyMine <razvlekuha300@gmail.com>
2026-02-21 15:58:36 +0300

    Minor fixes

--- src/main/java/net/lopymine/ip/t2o/Texture2ObjectsManager.java
+++ src/main/java/net/lopymine/ip/t2o/Texture2ObjectsManager.java
@@ -4,14 +4,12 @@ import java.io.InputStream;
 import java.util.*;
 import net.lopymine.ip.client.InventoryParticlesClient;
 import net.lopymine.ip.config.InventoryParticlesConfig;
-import net.lopymine.ip.utils.ArgbUtils2;
 import net.lopymine.mossylib.loader.MossyLoader;
 import net.lopymine.mossylib.utils.*;
 import net.minecraft.client.Minecraft;
 import com.mojang.blaze3d.platform.NativeImage;
 import net.minecraft.server.packs.resources.Resource;
 import net.minecraft.resources.Identifier;
-import net.minecraft.util.ARGB;
 
 public class Texture2ObjectsManager {
 
@@ -34,7 +32,7 @@ public class Texture2ObjectsManager {
 			int height = image.getHeight();
 			for (int x = 0; x < width; x++) {
 				for (int y = 0; y < height; y++) {
-					int color = /*? if <=1.21.1 {*/ /*ARGB.fromABGR(image.getPixelRGBA(x, y)); *//*?} else {*/ image.getPixel(x, y); /*?}*/
+					int color = /*? if <=1.21.1 {*/ /*fromABGR(image.getPixelRGBA(x, y)); *//*?} else {*/ image.getPixel(x, y); /*?}*/
 					if (Boolean.FALSE.equals(filter.getFilter().accept(x, y, width, height, color))) {
 						continue;
 					}
@@ -50,6 +48,14 @@ public class Texture2ObjectsManager {
 			InventoryParticlesClient.LOGGER.error("Failed to load create {} from texture \"{}\"! Reason:", id, e);
 		}
 		return List.of();
+	}
+
+	public static int toABGR(int color) {
+		return color & -16711936 | (color & 16711680) >> 16 | (color & 255) << 16;
+	}
+
+	public static int fromABGR(int color) {
+		return toABGR(color);
 	}
 
 }
